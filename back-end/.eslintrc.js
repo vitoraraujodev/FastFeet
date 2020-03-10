f3ea296d674e44fbbca48343e3729fdd70aebaa2
vitoraraujodev <vitoraraujo.dev@gmail.com>
2020-03-10 12:07:38 -0300

    Configurando estrutura

---
+++ back-end/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+  ],
+  rules: {
+    "prettier/prettier":  "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelCase": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
+  },
+};
