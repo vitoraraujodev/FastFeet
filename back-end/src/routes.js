9611b43c1baf90134317899dd4a0aad8ad702956
vitoraraujodev <vitoraraujo.dev@gmail.com>
2020-03-10 15:47:18 -0300

    Validando administrador

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 
@@ -7,7 +9,7 @@ const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
 
-routes.post('/recipients', RecipientController.store);
-routes.put('/recipients/:id', RecipientController.update);
+routes.post('/recipients', authMiddleware, RecipientController.store);
+routes.put('/recipients/:id', authMiddleware, RecipientController.update);
 
 export default routes;
