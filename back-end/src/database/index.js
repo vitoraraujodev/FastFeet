1208e9f7447f0d4fcdc4f803938b847d9ba132fc
vitoraraujodev <vitoraraujo.dev@gmail.com>
2020-03-12 11:10:40 -0300

    Sistema de armazenamento de arquivos

--- back-end/src/database/index.js
+++ back-end/src/database/index.js
@@ -2,10 +2,11 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient]; // Todos os models a serem carregados
+const models = [User, Recipient, File]; // Todos os models a serem carregados
 
 class Database {
   constructor() {
@@ -14,7 +15,9 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import authMiddleware from './app/middlewares/auth';
 
+import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+
+const upload = multer(multerConfig);
 
 const routes = new Router();
 
@@ -11,5 +17,7 @@ routes.post('/sessions', SessionController.store);
 
 routes.post('/recipients', authMiddleware, RecipientController.store);
 routes.put('/recipients/:id', authMiddleware, RecipientController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
