3e77c606d754d615887e758d0b8728348df3413d
vitoraraujodev <vitoraraujo.dev@gmail.com>
2020-03-12 18:21:22 -0300

    Cadastro de Entregas

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -9,6 +9,7 @@ import FileController from './app/controllers/FileController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import DeliveryController from './app/controllers/DeliveryController';
 
 const upload = multer(multerConfig);
 
@@ -23,6 +24,8 @@ routes.get('/deliverymen', authMiddleware, DeliverymanController.index);
 routes.post('/deliverymen', authMiddleware, DeliverymanController.store);
 routes.put('/deliverymen/:id', authMiddleware, DeliverymanController.update);
 routes.delete('/deliverymen/:id', authMiddleware, DeliverymanController.delete);
+
+routes.post('/deliveries', authMiddleware, DeliveryController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
