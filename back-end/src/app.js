7889c91c89c345a793fc2f9e970b0796bf92e4b9
Vitor Araujo <vitoraraujo.dev@gmail.com>
2020-04-01 15:04:24 -0300

    Adicionando cors

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 import routes from './routes';
 
@@ -7,12 +8,12 @@ import './database';
 class App {
   constructor() {
     this.server = express();
-
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
